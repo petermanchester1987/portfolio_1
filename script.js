621e86c5fac2b41890ad22583229aed5a974d867
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-07-23 17:48:19 +0100

    first commit of portfolio site

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("ITS WORKING!");
+
+let theme = localStorage.getItem("theme");
+
+if (theme === null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
+}
+
+let themeDots = document.getElementsByClassName("theme-dot");
+
+function setTheme(mode) {
+  if (mode === "light") {
+    document.getElementById("theme-style").href = "./default.css";
+  } else if (mode === "blue") {
+    document.getElementById("theme-style").href = "./blue.css";
+  } else if (mode === "green") {
+    document.getElementById("theme-style").href = "./green.css";
+  } else if (mode === "purple") {
+    document.getElementById("theme-style").href = "./purple.css";
+  }
+
+  localStorage.setItem("theme", mode);
+}
+
+for (let i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    console.log("option clicked " + mode);
+    setTheme(mode);
+  });
+}
